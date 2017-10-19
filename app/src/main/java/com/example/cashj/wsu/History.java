6245f238394e19fff7d446b1cea44d437904f000
aaronmad <madarisaaron@gmail.com>
2017-10-19 00:53:15 -0400

    Added buttons to history.xml

--- app/src/main/java/com/example/cashj/wsu/History.java
+++ app/src/main/java/com/example/cashj/wsu/History.java
@@ -1,18 +1,26 @@
 package com.example.cashj.wsu;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
 
 /**
  * Created by cashj on 9/20/2017.
  */
 
 public class History extends AppCompatActivity {
+// Modified by Aaron on 10/19/2017
+    Calendar mCurrentDate;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+
+
+        mCurrentDate = Calendar.getInstance();
     }
 }
