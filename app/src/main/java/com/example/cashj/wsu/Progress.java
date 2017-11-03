acd8508170af9184601ec34d4a4eac925ba69229
Jason Cash <cashjason1@gmail.com>
2017-11-03 01:29:15 -0400

    Finished the averages and individual question line graphs. Also set them to chronological order from oldest to newest

--- app/src/main/java/com/example/cashj/wsu/Progress.java
+++ app/src/main/java/com/example/cashj/wsu/Progress.java
@@ -48,6 +48,7 @@ import java.sql.Time;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -62,10 +63,13 @@ public class Progress extends AppCompatActivity implements View.OnClickListener,
     String ID;
     FirebaseUser user;
     FirebaseDatabase database;
-    ArrayList<Integer> R1, A, I, D, E, R2;
+    ArrayList<Integer> R1, A, I, D, E, R2, newR1, newA, newI, newD, newE, newR2;
+    ArrayList<Long> dates, datesSorted;
     DatabaseReference mDatabase;
+    GraphView graph2;
     SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
 
+//Created double array and organize from least to greatest by the millis of time stamps gottens
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,17 +84,24 @@ public class Progress extends AppCompatActivity implements View.OnClickListener,
         D = new ArrayList<>();
         E = new ArrayList<>();
         R2 = new ArrayList<>();
+        newR1 = new ArrayList<>();
+        newA = new ArrayList<>();
+        newI = new ArrayList<>();
+        newD = new ArrayList<>();
+        newE = new ArrayList<>();
+        newR2 = new ArrayList<>();
+        dates = new ArrayList<>();
+        datesSorted = new ArrayList<>();
+        graph2 = (GraphView) findViewById(R.id.graph2);
+        graph2.setVisibility(View.GONE);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-
-
         mDatabase.child("users/"+ID+"/PostGameEval").addValueEventListener(this);
-
-
+        mDatabase.child("users/"+ID+"/PostPracticeEval").addValueEventListener(this);
+        mDatabase.child("users/"+ID+"/PostBullpenEval").addValueEventListener(this);
     }
 
     public double getAve(ArrayList<Integer> arr){
         double val = 0;
-
         for (int i = 0; i < arr.size(); i++){
             val = val + arr.get(i);
         }
@@ -101,12 +112,20 @@ public class Progress extends AppCompatActivity implements View.OnClickListener,
     @Override
     public void onClick(View v) {
         int i = v.getId();
-
     }
 
     @Override
     public void onDataChange(DataSnapshot dataSnapshot) {
         for (DataSnapshot snap : dataSnapshot.getChildren()) {
+            Date date = null;
+            try {
+                date = sdf.parse(snap.getKey().toString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            long millis = date.getTime();
+
+            dates.add(millis);
             String s = snap.getValue().toString();
             s = s.substring(1, s.length()-1);
             List<String> list = new ArrayList<String>(Arrays.asList(s.split(", ")));
@@ -128,39 +147,71 @@ public class Progress extends AppCompatActivity implements View.OnClickListener,
                 }
             }
         }
-        System.out.println("R1 = " + R1);
-        System.out.println("A = " + A);
-        System.out.println("I = " + I);
-        System.out.println("D = " + D);
-        System.out.println("E = " + E);
-        System.out.println("R2 = " + R2);
+        updateGraph();
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
+    }
+
+    public void updateGraph(){
+        //System.out.println("Stuff  = " + dates.size() + datesSorted.size());
+        datesSorted.clear();
+        for (int i = 0; i < dates.size(); i++){
+            datesSorted.add(dates.get(i));
+        }
+        newR1.clear();
+        newA.clear();
+        newI.clear();
+        newD.clear();
+        newE.clear();
+        newR2.clear();
+        Collections.sort(datesSorted);
+        System.out.println("Dates = " + dates);
+        System.out.println("Dates Sorted = " + datesSorted);
+        for(int i = 0; i < dates.size(); i++){
+            for(int j = 0; j < datesSorted.size(); j++){
+                if(dates.get(i) == datesSorted.get(j)){
+                    newR1.add(R1.get(j));
+                    newA.add(A.get(j));
+                    newI.add(I.get(j));
+                    newD.add(D.get(j));
+                    newE.add(E.get(j));
+                    newR2.add(R2.get(j));
+                }
+            }
+        }
+
+
+
+        System.out.println("R1 = " + newR1);
+        System.out.println("A = " + newA);
+        System.out.println("I = " + newI);
+        System.out.println("D = " + newD);
+        System.out.println("E = " + newE);
+        System.out.println("R2 = " + newR2);
         GraphView graph = (GraphView) findViewById(R.id.graph);
+        graph.removeAllSeries();
         BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
                 new DataPoint(0,0),
-                new DataPoint(1, 0 + getAve(R1)),
-                new DataPoint(2, 0 + getAve(A)),
-                new DataPoint(3, 0 + getAve(I)),
-                new DataPoint(4, 0 + getAve(D)),
-                new DataPoint(5, 0 + getAve(E)),
-                new DataPoint(6, 0 + getAve(R2)),
+                new DataPoint(1, 0 + getAve(newR1)),
+                new DataPoint(2, 0 + getAve(newA)),
+                new DataPoint(3, 0 + getAve(newI)),
+                new DataPoint(4, 0 + getAve(newD)),
+                new DataPoint(5, 0 + getAve(newE)),
+                new DataPoint(6, 0 + getAve(newR2)),
                 new DataPoint(7, 0 )
         });
-//        BarGraphSeries<DataPoint> series2 = new BarGraphSeries<>(new DataPoint[]{
-//                new DataPoint(0,0),
-//                new DataPoint(7, 0 )
-//        });
         series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
             @Override
             public int get(DataPoint data) {
-                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+                return Color.rgb((int)Math.abs(data.getX()*255/4), (int) Math.abs(data.getY()*255/6), 100);
             }
         });
         series.setSpacing(25);
         series.setAnimated(true);
-        series.setDrawValuesOnTop(false);
-        series.setValuesOnTopColor(Color.WHITE);
         graph.addSeries(series);
-        //graph.addSeries(series2);
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
         staticLabelsFormatter.setHorizontalLabels(new String[] {"", "R", "A", "I", "D", "E", "R", ""});
         staticLabelsFormatter.setVerticalLabels(new String[] {"0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
@@ -173,29 +224,52 @@ public class Progress extends AppCompatActivity implements View.OnClickListener,
             public void onTap(Series series, DataPointInterface dataPoint) {
                 int value = (int)dataPoint.getX();
                 if (value == 1){
-                    Toast.makeText(Progress.this, "Series1: On Data Point clicked: "+value, Toast.LENGTH_SHORT).show();
+                    showQuestionGraph(newR1);
                 }else if(value == 2){
-                    Toast.makeText(Progress.this, "Series1: On Data Point clicked: "+value, Toast.LENGTH_SHORT).show();
+                    showQuestionGraph(newA);
                 }
                 else if(value == 3){
-                    Toast.makeText(Progress.this, "Series1: On Data Point clicked: "+value, Toast.LENGTH_SHORT).show();
+                    showQuestionGraph(newI);
                 }
                 else if(value == 4){
-                    Toast.makeText(Progress.this, "Series1: On Data Point clicked: "+value, Toast.LENGTH_SHORT).show();
+                    showQuestionGraph(newD);
                 }
                 else if(value == 5){
-                    Toast.makeText(Progress.this, "Series1: On Data Point clicked: "+value, Toast.LENGTH_SHORT).show();
+                    showQuestionGraph(newE);
                 }
                 else if(value == 6){
-                    Toast.makeText(Progress.this, "Series1: On Data Point clicked: "+value, Toast.LENGTH_SHORT).show();
+                    showQuestionGraph(newR2);
                 }
-
             }
         });
     }
 
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
+    public void showQuestionGraph(ArrayList<Integer> question){
 
+
+        System.out.println("Dates = " + dates);
+
+        graph2.setVisibility(View.VISIBLE);
+        graph2.removeAllSeries();
+        graph2.getViewport().setMaxY(10);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0,0 + question.get(0))
+        });
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0,0)
+        });
+
+        for (int i = 1; i < question.size(); i++){
+            series.appendData(new DataPoint(series.getHighestValueX()+1, 0 + question.get(i)), true, 40);
+        }
+        graph2.setMinimumHeight(10);
+        series.setDrawAsPath(true);
+        graph2.addSeries(series);
+        graph2.addSeries(series2);
+        series.setColor(Color.rgb(200,200,0));
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph2);
+        staticLabelsFormatter.setVerticalLabels(new String[] {"0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
+        graph2.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+        graph2.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.HORIZONTAL );
     }
 }
