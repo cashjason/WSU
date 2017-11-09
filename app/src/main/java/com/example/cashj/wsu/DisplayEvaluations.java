843dcec710661efc6814e34f8af88cea4c77980f
Jason Cash <cashjason1@gmail.com>
2017-11-08 19:53:59 -0500

    Finished the Display evals from the history section

--- app/src/main/java/com/example/cashj/wsu/DisplayEvaluations.java
+++ app/src/main/java/com/example/cashj/wsu/DisplayEvaluations.java
@@ -1,18 +1,13 @@
 package com.example.cashj.wsu;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
-import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,18 +15,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class DisplayEvaluations extends AppCompatActivity {
 
     String ID;
-    TextView title, q1, q2, q3, q4, q5,q6, bq1, bull1, bull2, bull3, bull4;
-    SeekBar rSeek, aSeek, iSeek, dSeek, eSeek, r2Seek, bSeek;
-    EditText notes, bull1Notes, bull2Notes, bull3Notes, bull4Notes;
+    TextView title, q1, q2, q3, q4, q5,q6, bq1, bull1, bull2, bull3, bull4, abr1, abr2, pitch1Q, pitch2Q, pitch3Q, pitch4Q, pitch5Q, pitch6Q;
+    SeekBar rSeek, aSeek, iSeek, dSeek, eSeek, r2Seek, bSeek, ab1Seek, ab2Seek, pitch1Seek, pitch2Seek;
+    EditText notes, bull1Notes, bull2Notes, bull3Notes, bull4Notes, ab1Notes, ab2Notes, ab3Notes,
+            ab4Notes, ab5Notes, ab6Notes, ab7Notes, pgn1, pgn2, pgn3, pgn4, aNotes;
     LinearLayout general, postGamePitcher, postGame, postBullpen;
     SimpleDateFormat simpleDateFormat;
     FirebaseDatabase database;
@@ -61,6 +53,19 @@ public class DisplayEvaluations extends AppCompatActivity {
         q4 = (TextView) findViewById(R.id.q4);
         q5 = (TextView) findViewById(R.id.q5);
         q6 = (TextView) findViewById(R.id.q6);
+        bq1 = (TextView) findViewById(R.id.bq1);
+        bull1 = (TextView) findViewById(R.id.bull1);
+        bull2 = (TextView) findViewById(R.id.bull2);
+        bull3 = (TextView) findViewById(R.id.bull3);
+        bull4 = (TextView) findViewById(R.id.bull4);
+        abr1 = (TextView) findViewById(R.id.abd1);
+        abr2 = (TextView) findViewById(R.id.abd2);
+        pitch1Q = (TextView) findViewById(R.id.pq1);
+        pitch2Q = (TextView) findViewById(R.id.pq2);
+        pitch3Q = (TextView) findViewById(R.id.pitch1Q);
+        pitch4Q = (TextView) findViewById(R.id.pitch2Q);
+        pitch5Q = (TextView) findViewById(R.id.pitch3Q);
+        pitch6Q = (TextView) findViewById(R.id.pitch4Q);
 
         rSeek = (SeekBar) findViewById(R.id.Rseek);
         aSeek = (SeekBar) findViewById(R.id.Aseek);
@@ -68,7 +73,28 @@ public class DisplayEvaluations extends AppCompatActivity {
         dSeek = (SeekBar) findViewById(R.id.Dseek);
         eSeek = (SeekBar) findViewById(R.id.Eseek);
         r2Seek = (SeekBar) findViewById(R.id.R2seek);
+        bSeek = (SeekBar) findViewById(R.id.bpSeek);
+        ab1Seek = (SeekBar) findViewById(R.id.abSeek);
+        ab2Seek = (SeekBar) findViewById(R.id.ab2Seek);
+        pitch1Seek = (SeekBar) findViewById(R.id.pitch1Seek);
+        pitch2Seek = (SeekBar) findViewById(R.id.pitch2Seek);
         notes = (EditText) findViewById(R.id.notesText);
+        bull1Notes = (EditText) findViewById(R.id.b1Notes);
+        bull2Notes = (EditText) findViewById(R.id.b2Notes);
+        bull3Notes = (EditText) findViewById(R.id.b3Notes);
+        bull4Notes = (EditText) findViewById(R.id.b4Notes);
+        ab1Notes = (EditText) findViewById(R.id.ab1Notes);
+        ab2Notes = (EditText) findViewById(R.id.ab2Notes);
+        ab3Notes = (EditText) findViewById(R.id.ab3Notes);
+        ab4Notes = (EditText) findViewById(R.id.ab4Notes);
+        ab5Notes = (EditText) findViewById(R.id.ab5Notes);
+        ab6Notes = (EditText) findViewById(R.id.ab6Notes);
+        ab7Notes = (EditText) findViewById(R.id.ab7Notes);
+        pgn1 = (EditText) findViewById(R.id.pitch1A);
+        pgn2 = (EditText) findViewById(R.id.pitch2A);
+        pgn3 = (EditText) findViewById(R.id.pitch3A);
+        pgn4 = (EditText) findViewById(R.id.pitch4A);
+        aNotes = (EditText) findViewById(R.id.aNotes);
 
         general = (LinearLayout) findViewById(R.id.general);
         postGame = (LinearLayout) findViewById(R.id.postGameHitter);
@@ -81,7 +107,6 @@ public class DisplayEvaluations extends AppCompatActivity {
         mDatabase.child("users/"+ID+"/PlayerInformation/Position").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + dataSnapshot.getValue());
                 if (dataSnapshot.getValue().toString().equals("Hitter")){
                     if (eval.equals("PostGameEval")){
                         postGame.setVisibility(View.VISIBLE);
@@ -101,15 +126,81 @@ public class DisplayEvaluations extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
-        });//End of UserData Query
-
+        });
+        mDatabase.child("Questions/General").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    if (snap.getKey().equals("question1")){
+                        q1.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("question2")){
+                        q2.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("question3")){
+                        q3.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("question4")){
+                        q4.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("question5")){
+                        q5.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("question6")){
+                        q6.setText(snap.getValue().toString());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        mDatabase.child("Questions/Batter").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    if (snap.getKey().equals("atBat1")){
+                        abr1.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("atBat2")){
+                        abr2.setText(snap.getValue().toString());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        mDatabase.child("Questions/Pitcher").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    if (snap.getKey().equals("bullpen")){
+                        bull1.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("bullpen1")){
+                        bull2.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("bullpen2")){
+                        bull3.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("bullpen3")){
+                        bull4.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("game1")){
+                        pitch1Q.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("game2")){
+                        pitch2Q.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("game3")){
+                        pitch3Q.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("game4")){
+                        pitch4Q.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("game5")){
+                        pitch5Q.setText(snap.getValue().toString());
+                    }else if (snap.getKey().equals("game6")){
+                        pitch6Q.setText(snap.getValue().toString());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
         mDatabase.child("users/"+ID+"/" + eval + "/" + date).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snap : dataSnapshot.getChildren()) {
-                    //System.out.println("3333333333333333333333333333333333333333333333333333333333     " + snap.getKey() + "Has the value : " + snap.getValue());
                     String key = snap.getKey();
-                    
                     if (key.equals("question1Answer")){
                         long val = (long) snap.getValue();
                         rSeek.setProgress((int) val);
@@ -130,13 +221,57 @@ public class DisplayEvaluations extends AppCompatActivity {
                         r2Seek.setProgress((int) val);
                     }else if(key.equals("notes")){
                         notes.setText((String)snap.getValue());
+                    }else if(key.equals("bullpenQuestion1Answer")){
+                        long val = (long) snap.getValue();
+                        bSeek.setProgress((int) val);
+                    }else if(key.equals("bullpenQuestion1Notes")){
+                        bull1Notes.setText((String)snap.getValue());
+                    }else if(key.equals("bullpenQuestion2Notes")){
+                        bull2Notes.setText((String)snap.getValue());
+                    }else if(key.equals("bullpenQuestion3Notes")){
+                        bull3Notes.setText((String)snap.getValue());
+                    }else if(key.equals("bullpenQuestion4Notes")){
+                        bull4Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat1Answer")){
+                        long val = (long) snap.getValue();
+                        ab1Seek.setProgress((int) val);
+                    }else if(key.equals("atBat2Answer")){
+                        long val = (long) snap.getValue();
+                        ab2Seek.setProgress((int) val);
+                    }else if(key.equals("atBat1Notes")){
+                        ab1Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat2Notes")){
+                        ab2Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat3Notes")){
+                        ab3Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat4Notes")){
+                        ab4Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat5Notes")){
+                        ab5Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat6Notes")){
+                        ab6Notes.setText((String)snap.getValue());
+                    }else if(key.equals("atBat7Notes")){
+                        ab7Notes.setText((String)snap.getValue());
+                    }else if(key.equals("pitchQuestion1Answer")){
+                        long val = (long) snap.getValue();
+                        pitch1Seek.setProgress((int) val);
+                    }else if(key.equals("pitchQuestion2Answer")){
+                        long val = (long) snap.getValue();
+                        pitch2Seek.setProgress((int) val);
+                    }else if(key.equals("pitchQuestion3NotesAnswer")){
+                        pgn1.setText((String)snap.getValue());
+                    }else if(key.equals("pitchQuestion4NotesAnswer")){
+                        pgn2.setText((String)snap.getValue());
+                    }else if(key.equals("pitchQuestion5NotesAnswer")){
+                        pgn3.setText((String)snap.getValue());
+                    }else if(key.equals("pitchQuestion6NotesAnswer")){
+                        pgn4.setText((String)snap.getValue());
                     }
                 }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
-        });//End of UserData Query
+        });
     }
-
 }
